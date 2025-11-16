9690af759f9999ec14310289bae2d02daf3f2c93
Svitlana Koturlash <svitlanakoturlash@MacBook-Air-Svitlana.local>
2025-11-16 20:43:18 +0200

    Switch to bcryptjs for Render deployment

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+cconst bcrypt = require('bcryptjs');
 
 let users = []; // тимчасове зберігання користувачів
 
